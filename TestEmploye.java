c2090eb894aafa3b5bc11b68695dbb29bd3cdfb9
bhargavanaidu1202-it <bhargavanaidu1202@gmail.com>
2026-08-04 15:32:47 +0530

    Add files via upload

---
+++ TestEmploye.java
@@ -0,0 +1,14 @@
+
+public class TestEmploye {
+    public static void main(String[] args) {
+        
+    
+ Employee e=new Employee();
+ e.setName("BBN");   
+e.setSalary(30000);
+e.setNo("25A1202");
+e.setYear(2024);
+e.display();
+}
+
+}
